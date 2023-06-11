e965fe2e11ce1a86bf91d572f09b21f9255980da
Howllian27 <67510209+howllian27@users.noreply.github.com>
2023-06-11 15:55:43 +0800

    commit

--- App/interests/src/components/Desktop1/Component1_StateChecked/VectorIcon.tsx
+++
@@ -1,9 +0,0 @@
-import { memo, SVGProps } from 'react';
-
-const VectorIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg preserveAspectRatio='none' viewBox='0 0 34 24' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-    <path d='M11.9 23.7L0.7 12.5L2.85 10.35L11.9 19.4L31.1 0.2L33.25 2.35L11.9 23.7Z' fill='white' />
-  </svg>
-);
-const Memo = memo(VectorIcon);
-export { Memo as VectorIcon };
--- App/profile_creation/src/components/Desktop1/VectorIcon2.tsx
+++ App/profile_creation/src/components/Desktop1/VectorIcon2.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { memo, SVGProps } from 'react';
 
 const VectorIcon2 = (props: SVGProps<SVGSVGElement>) => (
